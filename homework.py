90cc1505e1ebcf14f204b41fe1cf989797ef69cd
AdhritShetty <adhritshetty2009@gmail.com>
2025-03-30 07:01:07 +0000

    homework

---
+++ homework.py
@@ -0,0 +1,8 @@
+text=str("congratulations")
+print("congratulations")
+#Reverse string
+#Using step value as -1 to iteratein reverse
+revText=text[::-1]
+text=revText
+print("reverse of given string is:")
+print(text)
